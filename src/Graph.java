da306741c91bc3f9688c56a443b2e6b53ecf5ee7
marilena <marilena.oita@gmail.com>
2014-04-05 12:51:48 +0200

    loading data

--- src/Graph.java
+++ src/Graph.java
@@ -9,70 +9,149 @@ import java.util.LinkedList;
 import java.util.List;
 
 
+
+/**
+ * loading data from file
+ * @author mari
+ *
+ */
 public class Graph {
-	private int nbIntersections; 
-	private int nbOfStreets; 
-	private int nbOfCars; 
-	private int nbOfSeconds; 
-	
-	List<Intersection> allIntersections = new LinkedList<Intersection>(); 
-	List<Street> allStreets = new LinkedList<Street>(); 
-	
-	private  void loadGraphData(String path) throws IOException{
-	FileInputStream istream = null;
+	private int nbIntersections;
+	private int nbOfStreets;
+	private int nbOfCars;
+	private int nbOfSeconds;
+	private int startingPoint;
 
-	// read the other file
-	istream = new FileInputStream(path);
-	DataInputStream in = new DataInputStream(istream);
-	BufferedReader br = new BufferedReader(new InputStreamReader(in));
+	List<Intersection> allIntersections = new LinkedList<Intersection>();
+	List<Street> allStreets = new LinkedList<Street>();
 
-	String strLine;
+	public int getNbIntersections() {
+		return nbIntersections;
+	}
 
-	strLine = br.readLine(); // first line
-	String[] initialData = strLine.split(" "); 
-	nbIntersections = Integer.valueOf(initialData[0]); 
-	nbOfStreets = Integer.valueOf(initialData[1]); 
-	nbOfCars = Integer.valueOf(initialData[2]); 
-	nbOfSeconds = Integer.valueOf(initialData[3]); 
+	public void setNbIntersections(int nbIntersections) {
+		this.nbIntersections = nbIntersections;
+	}
 
-	System.out.println("nbIntersections= " + nbIntersections + " , nbOfStreets = " + nbOfStreets + ", nbOfCars=" + nbOfCars + ", nbOfSeconds=" + nbOfSeconds);
+	public int getNbOfStreets() {
+		return nbOfStreets;
+	}
 
+	public void setNbOfStreets(int nbOfStreets) {
+		this.nbOfStreets = nbOfStreets;
+	}
 
-	//int id = 1;
-	try {
-		while ((strLine = br.readLine()) != null) {
-			if (strLine.trim().length() == 2) {
-				String[] intersectionData = strLine.split(" "); 
-				Intersection i = new Intersection(Integer.valueOf(intersectionData[2]), Integer.valueOf(intersectionData[0]), Integer.valueOf(intersectionData[1])); 
-				allIntersections.add(i); 
-			}else{
-				if (strLine.trim().length() == 5) {
-					String[] streetData = strLine.split(" "); 
-					int idxIntersectionBegin = Integer.valueOf(streetData[0]); 
-					int idxIntersectionEnd = Integer.valueOf(streetData[1]); 
-					int waysVal = Integer.valueOf(streetData[2]);
-					boolean oneway = waysVal ==1?true:false;
-					Street s = new Street(allIntersections.get(idxIntersectionBegin), allIntersections.get(idxIntersectionEnd), oneway, 
-							Integer.valueOf(streetData[3]), Integer.valueOf(streetData[4])); 
-					allStreets.add(s); 
+	public int getNbOfCars() {
+		return nbOfCars;
+	}
+
+	public void setNbOfCars(int nbOfCars) {
+		this.nbOfCars = nbOfCars;
+	}
+
+	public int getNbOfSeconds() {
+		return nbOfSeconds;
+	}
+
+	public void setNbOfSeconds(int nbOfSeconds) {
+		this.nbOfSeconds = nbOfSeconds;
+	}
+
+	public int getStartingPoint() {
+		return startingPoint;
+	}
+
+	public void setStartingPoint(int startingPoint) {
+		this.startingPoint = startingPoint;
+	}
+
+	public List<Intersection> getAllIntersections() {
+		return allIntersections;
+	}
+
+	public void setAllIntersections(List<Intersection> allIntersections) {
+		this.allIntersections = allIntersections;
+	}
+
+	public List<Street> getAllStreets() {
+		return allStreets;
+	}
+
+	public void setAllStreets(List<Street> allStreets) {
+		this.allStreets = allStreets;
+	}
+
+	private void loadGraphData(String path) throws IOException {
+		FileInputStream istream = null;
+
+		// read the other file
+		istream = new FileInputStream(path);
+		DataInputStream in = new DataInputStream(istream);
+		BufferedReader br = new BufferedReader(new InputStreamReader(in));
+
+		String strLine;
+
+		strLine = br.readLine(); // first line
+		String[] initialData = strLine.split(" ");
+		nbIntersections = Integer.valueOf(initialData[0]);
+		nbOfStreets = Integer.valueOf(initialData[1]);
+		nbOfSeconds = Integer.valueOf(initialData[2]);
+		nbOfCars = Integer.valueOf(initialData[3]);
+		startingPoint = Integer.valueOf(initialData[4]);
+
+		System.out.println("nbIntersections= " + nbIntersections
+				+ " , nbOfStreets = " + nbOfStreets + ", nbOfCars=" + nbOfCars
+				+ ", nbOfSeconds=" + nbOfSeconds + ", startingPoint="
+				+ startingPoint);
+
+		int id = 1;
+		try {
+			while ((strLine = br.readLine()) != null) {
+				if (strLine.trim().length() != 0) {
+					String[] lineData = strLine.split(" ");
+					if (lineData.length == 2) {
+						Intersection i = new Intersection(id,
+								Double.valueOf(lineData[0]),
+								Double.valueOf(lineData[1]));
+						allIntersections.add(i);
+						id++;
+					} else {
+						if (strLine.trim().length() == 5) {
+							int idxIntersectionBegin = Integer
+									.valueOf(lineData[0]);
+							int idxIntersectionEnd = Integer
+									.valueOf(lineData[1]);
+							int waysVal = Integer.valueOf(lineData[2]);
+							boolean oneway = waysVal == 1 ? true : false;
+							Street s = new Street(
+									allIntersections.get(idxIntersectionBegin),
+									allIntersections.get(idxIntersectionEnd),
+									oneway, Integer.valueOf(lineData[3]),
+									Integer.valueOf(lineData[4]));
+							allStreets.add(s);
+						}
+					}
 				}
 			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
 		}
-	} catch (Exception ex) {
-		ex.printStackTrace();
+
+		br.close();
 	}
 
-	br.close();
-	}
-	
-	//fast test
+	// fast test
 	public static void main(String[] args) {
-		Graph data = new Graph(); 
+		Graph data = new Graph();
 		try {
 			data.loadGraphData("/home/mari/Desktop/HASHLOFT/paris_54000.txt");
+			System.out.println("nbIntersections = "
+					+ data.getAllIntersections().size() + " ==? "
+					+ data.getNbIntersections());
+			System.out.println(data.getNbOfStreets());
 		} catch (IOException e) {
 			e.printStackTrace();
-		} 
+		}
 	}
-	
+
 }
