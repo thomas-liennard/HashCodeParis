367d0102b12d82e24df2fac1fb76139acd5b3ad0
electrolab <electrolab@dalamud>
2014-04-05 12:02:04 +0200

    added classes for streets and intersections

---
+++ src/Intersection.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Intersection {
+	public double latitude;
+	public double longitude;
+	public int id;
+	List<Street> streetsFrom = new ArrayList<Street>();
+	List<Street> streetsTo = new ArrayList<Street>();
+
+}
---
+++ src/Street.java
@@ -0,0 +1,9 @@
+
+public class Street {
+	public Intersection begin;
+	public Intersection end;
+	int cost;
+	int length;
+	boolean oneWays;
+
+}
