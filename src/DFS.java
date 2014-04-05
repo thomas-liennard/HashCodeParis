404ed8e3b146923e9a37797cab715ba7e4e8eae1
mmhancxt <mmhancxt@gmail.com>
2014-04-05 16:49:44 +0200

    sadf

--- src/DFS.java
+++ src/DFS.java
@@ -11,15 +11,15 @@ public class DFS {
 	 * @param args
 	 */
 	public static int TIMELIMIT = 54000;
-	
+
 	static int length = 0;
 	static int cost = 0;
-	static int numbers =1;
+	static int numbers = 1;
 	static boolean finished = false;
 	GraphData g;
 	FileWriter fw;
 	BufferedWriter bw;
-	
+
 	public void closefile() {
 		try {
 			bw.close();
@@ -29,9 +29,9 @@ public class DFS {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public DFS() {
-		g= new GraphData();
+		g = new GraphData();
 		try {
 			fw = new FileWriter("output.txt");
 			bw = new BufferedWriter(fw);
@@ -48,76 +48,114 @@ public class DFS {
 			System.out.println("nbIntersections = "
 					+ g.getAllIntersections().size() + " ==? "
 					+ g.getNbIntersections());
-			
-			List<Street> streets = g.getAllStreets(); 
-			System.out.println("nbStreets=" + streets.size() + " ==? " + g.getNbOfStreets());
-		
+
+			List<Street> streets = g.getAllStreets();
+			System.out.println("nbStreets=" + streets.size() + " ==? "
+					+ g.getNbOfStreets());
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public void process_edge(Street edge) {
-		cost += edge.getCost();
-		numbers ++;
-		if (!edge.visited) {
-			length += edge.getLength();
-			edge.visited = true;
-		}
-		try {
-			bw.write(edge.getBegin().getId()+"");
-			bw.newLine();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+
+	public boolean process_edge(Street edge) {
+		if (cost + edge.getCost() > TIMELIMIT)
+			return false;
+		else {
+			cost += edge.getCost();
+			numbers++;
+			if (!edge.visited) {
+				length += edge.getLength();
+				edge.visited = true;
+			}
+			try {
+				System.out.println(edge.getBegin().getId());
+				System.out.println("Cost now is : " + cost);
+				if (cost == 20642) {
+					System.out.println("debug here");
+				}
+				bw.write(edge.getBegin().getId() + "");
+				bw.newLine();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return true;
 		}
 	}
-	
-	Street select_edge(Intersection vertex) {
-		if (vertex.getStreetsFrom().size() != 0) {
-			
-		}
-	}
-	
-	public void dfs(Intersection vertex) {
-		Intersection y;
-		vertex.discovered=true;
-		
-		Street edge = select_edge(vertex);
-		
-		for (Street edge : vertex.getStreetsFrom()) {
-			if (finished) break;
-			y = edge.getEnd();
-			if (y.discovered == false) {
-				y.parent = vertex;
-				
-				process_edge(edge);
-				
-				if (cost < TIMELIMIT)
-					dfs(y);
-				else {
-					length -= edge.getLength();
-					finished = true;
+
+	int select_edge(Intersection vertex) {
+		float maxSpeed = -1;
+		int result = -1;
+		int minTime = 54000;
+		int timeResult = -1;
+		for (int i = 0; i < vertex.getStreetsFrom().size(); i++) {
+			Street street = vertex.getStreetsFrom().get(i);
+			if (!street.visited) {
+				if (street.speed > maxSpeed) {
+					maxSpeed = street.speed;
+					result = i;
+				}
+			} else {
+				if (street.getCost() < minTime) {
+					minTime = street.getCost();
+					timeResult = i;
 				}
 			}
 		}
+		if (result == -1) {
+			return timeResult;
+		} else {
+			return result;
+		}
+
 	}
-	
+
+	public void dfs(Intersection vertex) {
+		Intersection y;
+		vertex.discovered = true;
+
+		int x=select_edge(vertex);
+		System.out.println("x=" + x);
+		Street edge = vertex.getStreetsFrom().get(x);
+		y = edge.getEnd();
+		
+		if (process_edge(edge))
+			dfs(y);
+		else
+			return;
+		/*
+		if (y.discovered == false) {
+			y.parent = vertex;
+
+			process_edge(edge);
+
+			if (cost < TIMELIMIT)
+				dfs(y);
+			else {
+				length -= edge.getLength();
+				finished = true;
+			}
+		}*/
+
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		DFS d = new DFS();
-		Intersection start = d.g.getAllIntersections().get(d.g.getStartingPoint());
-		
-		System.out.println(d.g.getStartingPoint());
+		Intersection start = d.g.getAllIntersections().get(
+				d.g.getStartingPoint());
+
+		Intersection view = d.g.getAllIntersections().get(2420);
 		System.out.println("Streets from: ");
-		for (Street s : start.getStreetsFrom()) {
+		for (Street s : view.getStreetsFrom()) {
 			System.out.println(s.getEnd().getId());
 		}
-		
+
 		System.out.println("Streets to:");
-		for (Street s : start.getStreetsTo() ) 
+		for (Street s : view.getStreetsTo())
 			System.out.println(s.getBegin().getId());
-		
+
 		d.dfs(start);
 		d.closefile();
 		System.out.println("Totol time is : " + cost);
